87226a727cfd03f2abbe42aed6c5879ae7e1e82e
Eya-Benhouria <benhouriaeya@gmail.com>
2024-07-26 02:18:33 +0200

    S7 AM finished

---
+++ AM.ApplicationCore/Domain/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    public class Ticket
+    {
+        public String classe { get; set; }
+        public String destination { get; set; }
+        public int TicketId { get; set; }
+        public ReservationTicket Reservation { get; set; }
+
+    }
+}
---
+++ AM.Infrastructure/AMContext.cs
@@ -0,0 +1,57 @@
+﻿using AM.ApplicationCore.Domain;
+using AM.Infrastructure.Configurations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.Infrastructure
+{
+    public class AMContext:DbContext
+    {
+        public DbSet<Flight> Flights { get; set; }
+        public DbSet<Plane> Planes { get; set; }
+        public DbSet<Passenger> Passengers { get; set; }
+        public DbSet<Staff> Staffs { get; set; }
+        public DbSet<Traveller> Travellers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+                optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;
+                       Initial Catalog=AirportManagement11;
+                       Integrated Security=true;MultipleActiveResultSets=true");
+
+            base.OnConfiguring(optionsBuilder);
+        }
+        //appel des classes de config 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new PlaneConfiguration());
+            modelBuilder.ApplyConfiguration(new FlightConfiguration());
+            //2 ème methode de fluent API et 2 eme de config de typeC <=> [Owned]
+            modelBuilder.Entity<Passenger>().OwnsOne(p => p.FullName, full =>
+            {
+                full.Property(p => p.FirstName).IsRequired();
+                full.Property(p => p.LastName);
+
+            });
+
+
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder){
+            base.ConfigureConventions(configurationBuilder);
+            configurationBuilder.Properties<DateTime>().HaveColumnType("date");
+
+        }
+
+
+        
+    }
+}
